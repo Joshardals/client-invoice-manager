1343490c68bac3bf235bb3ab528ffb7957226668
Joshardals <joshuabamidele219@gmail.com>
2025-05-20 04:14:51 +0100

    initial commit

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,4 +1,9 @@
 import { Dashboard } from "@/components/Dashboard/Dashboard";
+import { Viewport } from "next";
+
+export const viewport: Viewport = {
+  themeColor: "black",
+};
 
 export default async function DashboardPage() {
   return <Dashboard />;
